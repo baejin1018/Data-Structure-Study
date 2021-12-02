c74c115586257687755aa33f619c0b2425206acb
Bae-Jin-Young <bae20051018@gmail.com>
2021-12-02 22:10:18 +0900

    1-3 자료구조

---
+++ python/숫자만 보여주세요.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+n = list(input())
+num = 0
+for i in range(len(n)):
+	if(ord(n[i])< 65):
+		num += int(n[i])
+		num *= 10
+num //= 10
+num += 5
+print(num)
